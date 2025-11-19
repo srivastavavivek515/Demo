654821e9e07324212daeead9f0f0eed3dbe2aa15
srivastavavivek515 <srivastavavivek515@gmail.com>
2025-11-19 18:01:13 +0530

    master1

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     DemoTheme {
-        Greeting("Android")
+        Greeting("master 1")
     }
 }
